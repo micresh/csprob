aa7005a5b622ea07770496a5d91ef64d7f56c97f
Mikhail Reshetnyak <micresh@gmail.com>
2018-11-22 22:46:47 +0300

    add numeric function to convertation base problems

--- problems.py
+++ problems.py
@@ -1,4 +1,5 @@
 import numpy as np
+from supplyfunctions import convert_base
 
 
 def inf_counting_audio_filesize(max_minutes):
@@ -12,7 +13,7 @@ def inf_counting_audio_filesize(max_minutes):
     pr = "Выполнена " + str(curr_chan) + \
          "-канальная запись звука с частотой дискретизации " + str(curr_freq) + \
          "кГц и разрешением " + str(curr_bits) + " бит. Какой объем (в мегабайтах) занимает файл," \
-                                              " если запись проводилась " + str(time)
+                                                 " если запись проводилась " + str(time)
     if time < 5:
         pr += " минуты"
     else:
@@ -20,24 +21,21 @@ def inf_counting_audio_filesize(max_minutes):
     return pr, curr_chan * (curr_bits / 8) * curr_freq * 1000 * time * 60 // 2 ** 20
 
 
-def inf_counting_audio_time(problems_number):
-    ans = []
+def inf_counting_audio_time(min_fs, max_fs):
     freq = [16, 32, 64, 128, 192, 256]
     chan = [2, 4, 8]
     bits = [8, 16, 32]
-    for i in range(problems_number):
-        curr_chan = np.random.choice(chan)
-        curr_freq = np.random.choice(freq)
-        curr_bits = np.random.choice(bits)
-        filesize = np.random.randint(100, 300)
-        pr = "Выполнена " + str(curr_chan) + \
-             "-канальная запись звука с частотой дискретизации " + str(curr_freq) + \
-             "кГц и разрешением " + str(curr_bits) + " бит. В результате получился файл объемом " \
-             + str(filesize) + " мегабайт. Определите сколько времени в секундах" \
-                               " длилась запись (количество секунд - целое)"
-        ans.append(pr)
-        ans.append(np.around(filesize*2**23/curr_chan/curr_bits/curr_freq/1000))
-    return ans
+    curr_chan = np.random.choice(chan)
+    curr_freq = np.random.choice(freq)
+    curr_bits = np.random.choice(bits)
+    fs = np.random.randint(min_fs, max_fs)
+    pr = "Выполнена " + str(curr_chan) + \
+         "-канальная запись звука с частотой дискретизации " + str(curr_freq) + \
+         "кГц и разрешением " + str(curr_bits) + " бит. В результате получился файл объемом " \
+         + str(fs) + " мегабайт. Определите сколько времени в секундах" \
+                     " длилась запись (количество секунд - целое)"
+
+    return pr, np.around(fs * 2 ** 23 / curr_chan / curr_bits / curr_freq / 1000)
 
 
 def inf_counting_image_size(problems_number):
@@ -53,3 +51,12 @@ def inf_counting_image_colors(problems_number):
 
 def inf_counting_passwords():
     return
+
+
+def numeric_notation_convert_number(from_base=10, to_base=2, min_number=2, max_number=500):
+    gen_number = np.random.randint(min_number, max_number)
+    res_number = convert_base(gen_number, to_base)
+    pr = "$ " + convert_base(gen_number, from_base) + \
+         "_{" + str(from_base) + "} = X_{" + str(to_base) + "}$"
+    return pr, res_number
+
